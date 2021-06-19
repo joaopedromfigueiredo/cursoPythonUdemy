64bcb4adfff3b19f8f06720d470aae81be03a223
João Pedro Figueiredo <76779567+joaopedromfigueiredo@users.noreply.github.com>
2021-06-18 21:38:24 -0300

    Update desafio_final_validador_cpf.py

--- 001_basico/desafio_final_validador_cpf.py
+++ 001_basico/desafio_final_validador_cpf.py
@@ -1,11 +1,3 @@
-'''
-Bugs percebidos no algoritmo:
-
-* O algoritmo imprime CPF inválido para todos os CPFs terminados em 0, mesmo que seja um CPF válido.
-
-'''
-
-
 cpf = input('Digite seu cpf, apenas os números: ')
 soma1 = 0
 soma2 = 0
@@ -14,26 +6,29 @@ soma2 = 0
 
 # Fazendo a multiplicação dos nove primeiros por 10, 9, 8 [...] até o 2 e gerando uma soma:
 for i, v in enumerate(range(10, 1, -1)):
-
     soma1 += int(cpf[i]) * v
 
 # Gerando o décimo digito
 condicao_digito10 = 11 - (soma1 % 11)
+
 if condicao_digito10 > 9:
     digito10 = 0
 else:
     digito10 = condicao_digito10
 
-
 # Conferindo o décimo-primeiro dígito do CPF
-
 # Fazendo a multiplicação dos dez primeiros por 11, 10, 9 [...] até o 2 e gerando uma soma:
 for j, k in enumerate(range(11, 1, -1)):
-
     soma2 += int(cpf[j]) * k
 
-condicao_digito11 = 11 - (soma2 % 11)  # Gerando o décimo primeiro dígito
+# Gerando o décimo primeiro dígito
+condicao_digito11 = 11 - (soma2 % 11)
 digito11 = condicao_digito11
+
+if condicao_digito11 > 9:
+    digito11 = 0
+else:
+    digito11 = condicao_digito11
 
 novo_cpf = ''
 
