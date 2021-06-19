d9475b55b43dbadad889bd9095989a1045b053dd
João Pedro Figueiredo <76779567+joaopedromfigueiredo@users.noreply.github.com>
2021-06-19 03:26:30 -0300

    Create 001_def.py

---
+++ 002_intermediario_procedural/001_def.py
@@ -0,0 +1,25 @@
+# o def é um método que cria uma função em Python
+
+def soma(a, b):
+    
+    a = int(input('Digite um número: '))
+    b = int(input('Digite outro número: '))
+
+    return a + b
+
+
+n1, n2 = 0, 0
+resultado1 = soma(n1, n2)
+print(resultado1, '\n')
+
+n3, n4 = 0, 0
+resultado2 = soma(n3, n4)
+print(resultado2, '\n')
+
+
+
+'''
+
+A vantagem de usar funções é que não há necessidade de repetição de código para as mesmas tarefas
+
+'''
